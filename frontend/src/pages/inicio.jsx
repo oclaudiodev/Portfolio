5d16654155671ff375878315211f418a5f51dac8
oclaudiodev <7claudiosouza@gmail.com>
2025-11-09 16:39:23 -0300

    responsividade

--- frontend/src/pages/inicio.jsx
+++ frontend/src/pages/inicio.jsx
@@ -5,7 +5,7 @@ export default function Inicio() {
         <div className="inicio">
             <section className="apresentacao">
                 <div className="texto">
-                    <h1>👋 Olá, eu sou <span>Cláudio</span></h1>
+                    <h1> Olá, eu sou <span>Cláudio</span></h1>
                     <h2>Desenvolvedor Full Stack</h2>
                     <p>
                         Transformo ideias em soluções completas. Desenvolvo aplicações do front ao back-end, unindo design intuitivo e arquitetura robusta. Utilizo tecnologias como <strong>React</strong>, <strong>Node.js Express</strong>, <strong>MySQL </strong> e <strong>SCSS</strong>, sempre com foco em performance, escalabilidade e experiência do usuário. 
@@ -26,7 +26,7 @@ export default function Inicio() {
                             rel="noopener noreferrer"
                             className="btn btn-secundario"
                         >
-                            💬 Contato
+                             Contato
                         </a>
                     </div>
                 </div>
@@ -37,7 +37,7 @@ export default function Inicio() {
             </section>
 
             <section id="projetos" className="projetos">
-                <h2>💡 Projetos em Destaque</h2>
+                <h2> Projetos em Destaque</h2>
                 <div className="cards">
                     <div className="card">
                         <h3>Dashboard de Inscrições</h3>
@@ -48,14 +48,14 @@ export default function Inicio() {
                         <p>Design moderno, animações suaves e total responsividade.</p>
                     </div>
                     <div className="card">
-                        <h3>App de Denúncias</h3>
-                        <p>Sistema completo para envio e controle de denúncias.</p>
+                        <h3>Conectando Gerações (TCC T.I)</h3>
+                        <p>Website completo desenvolvido como Trabalho de Conclusão de Curso em T.I., utilizando React, Node.js e MySQL. O projeto tem como objetivo conectar diferentes gerações por meio de uma plataforma interativa e acessível.</p>
                     </div>
                 </div>
             </section>
 
             <section id="contato" className="contato">
-                <h2>📩 Entre em Contato</h2>
+                <h2> Entre em Contato</h2>
                 <p>Vamos conversar sobre oportunidades e novos projetos!</p>
                 <a
                     href="https://mail.google.com/mail/?view=cm&fs=1&to=7claudiosouza@gmail.com&su=Contato%20via%20Portfólio&body=Olá%20Cláudio%2C%20vi%20seu%20portfólio%20e%20gostaria%20de%20falar%20sobre..."
